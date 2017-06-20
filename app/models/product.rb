b16618f41c5f05fde7027785d588d0db62e9acf8
Ziad Zorgati <Ziad.Zorgati@gmx.de>
2017-06-20 06:09:28 +0200

    added similar product feature  - product tag model, new scopes for the find similar feature  - product model, new methodes for the find similar feature  - modify show template to use similar feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,30 @@ class Product < ApplicationRecord
   def product_tags_count
     product_tags.size
   end
+
+  def product_tags_names
+    product_tags.map(&:name)
+  end
+
+  def five_most_similar_products
+    product_id_with_matching_tags_count.sort_by { |_key, value| value }.reverse[0..4]
+  end
+
+  private
+
+  def all_products_ids_with_same_product_tags
+    product_tags_names.map do |product_tags_name|
+      ProductTag.products_id_with_tag_name(product_tags_name)
+    end.flatten.reject { |elm| elm == id }
+  end
+
+  def product_id_with_matching_tags_count
+    product_id_matching_tags = Hash.new(0)
+
+    all_products_ids_with_same_product_tags.each do |product_id|
+      product_id_matching_tags[product_id] += 1
+    end
+
+    product_id_matching_tags
+  end
 end
--- app/models/product_tag.rb
+++ app/models/product_tag.rb
@@ -3,4 +3,8 @@ class ProductTag < ApplicationRecord
 
   validates :product, presence: true
   validates :name, uniqueness: { scope: :product_id }
+
+  scope :select_tags_by_name, ->(tag_name) { where(name: tag_name) }
+  scope :count_used_over_all_product_with_tag_name, ->(tag_name) { select_tags_by_name(tag_name).count }
+  scope :products_id_with_tag_name, ->(tag_name) { select_tags_by_name(tag_name).pluck(:product_id) }
 end
